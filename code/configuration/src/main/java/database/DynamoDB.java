61721d2a25cd32223f22a11da3843dbdb19e6b42
Rafael <rafael.castilhos@sparkag.com.br>
2023-02-23 15:46:08 -0300

    add aws credentials provider

--- code/configuration/src/main/java/database/DynamoDB.java
+++ code/configuration/src/main/java/database/DynamoDB.java
@@ -1,15 +1,20 @@
 package database;
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 public class DynamoDB {
     private static DynamoDbEnhancedClient INSTANCE_MAPPER;
-
+    static AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
+            "AKIA4AYSRE4ZHSIRLK53",
+            "IgM4D5uyuSxbXtpzlxaEh9jaUzgIcQL+6j0ct17S");
     private static final DynamoDbClient mapper = DynamoDbClient
             .builder()
             .region(Region.US_EAST_1)
+            .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
             .build();
 
     public static DynamoDbEnhancedClient getInstanceDynamoDb() {
