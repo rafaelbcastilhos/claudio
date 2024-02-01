2502acb5f5739828296a68b97d279a6df6341ed2
Rafael Castilhos <rafael.castilhos@sparkag.com.br>
2024-02-01 17:10:07 -0300

    atualizando arrays main

--- code/plots/main.py
+++ code/plots/main.py
@@ -2,19 +2,22 @@ import repository
 import statistics
 
 
-json = repository.get_method_size("JSON", "M")
-xml = repository.get_method_size("XML", "S")
-msgpack = repository.get_method_size("MSGPACK", "S")
-kryo = repository.get_method_size("KRYO", "S")
+json = repository.get_method_size("JSON", "S")
+#xml = repository.get_method_size("XML", "B")
+#msgpack = repository.get_method_size("MSGPACK", "S")
+#kryo = repository.get_service_size("EC2", "B")
 
+j_request = []
+j_serialize = [] 
+j_deserialize = []
+sum_deserialize = 0
 for j in json:
-    j_request = []
-    j_serialize = [] 
-    j_deserialize = []
+    sum_deserialize = sum_deserialize + j['timeDeserialize']
     j_request.append(j['timeRequest'])
     j_serialize.append(j['timeSerialize'])
     j_deserialize.append(j['timeDeserialize'])
 
-print(f"request: {statistics.median(j_request)}")
-print(f"serialize: {statistics.median(j_serialize)}")
-print(f"deserialize: {statistics.median(j_deserialize)}")
+#print(f"request: {(j_request)}")
+#   print(f"serialize: {j_serialize}")
+print(f"deserialize: {sum_deserialize / len(j_deserialize)}")
+
