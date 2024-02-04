7109741a529cc10c48e9558256e96a42d2d09a16
Rafael Castilhos <rafael.castilhos@sparkag.com.br>
2024-02-04 20:22:13 -0300

    add more plots

--- code/plots/plot.py
+++ code/plots/plot.py
@@ -1,15 +1,40 @@
 import matplotlib.pyplot as plt
 
-def method_size(categories, values, size):
+def method_size(categories, values, size, ylabel):
     plt.bar(categories, values, color='blue')
 
     # Adicionando rótulos e título
     plt.xlabel('Formato dos dados')
-    plt.ylabel('Tempo médio da requisição (ms)')
+    plt.ylabel(ylabel)
     plt.title(f'Gráfico de Barras para tamanho {size}')
 
     for i, v in enumerate(values):
         plt.text(i, v + 0.1, "{:.2f}".format(v), ha='center', va='bottom')
 
-    # Exibindo o gráfico
+    plt.show()
+
+def method_service(categories, values, service, ylabel):
+    plt.bar(categories, values, color='blue')
+
+    # Adicionando rótulos e título
+    plt.xlabel('Formato dos dados')
+    plt.ylabel(ylabel)
+    plt.title(f'Gráfico de Barras para o serviço {service}')
+
+    for i, v in enumerate(values):
+        plt.text(i, v + 0.1, "{:.2f}".format(v), ha='center', va='bottom')
+
+    plt.show()
+
+def method_type(categories, values, typeData, ylabel):
+    plt.bar(categories, values, color='blue')
+
+    # Adicionando rótulos e título
+    plt.xlabel('Formato dos dados')
+    plt.ylabel(ylabel)
+    plt.title(f'Gráfico de Barras para a formatação do tipo {typeData}')
+
+    for i, v in enumerate(values):
+        plt.text(i, v + 0.1, "{:.2f}".format(v), ha='center', va='bottom')
+
     plt.show()
